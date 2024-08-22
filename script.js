842f23ae55c20a0cb8b75e9678d958e37a27be3d
Ankit Kumaar <ankitkumaar040399@gmail.com>
2024-08-22 21:33:41 +0530

    update commit

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+// right click disabled
+var message = "Access denied! No copying allowed!";
+function rtclickcheck(keyp) { 
+    if (navigator.appName == "Netscape" && keyp.which == 3) { 
+        alert(message); return false; 
+    }
+    if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { 
+        alert(message); return false; 
+    } 
+}
+document.onmousedown = rtclickcheck;
+
 // contact me - form data to excel sheet
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyqX_olSFGLzbgKb0ubgR5hXb2r_pOkYemXsJAYmzmAy5MgE3RruwMRpnvLKfkjw1uK/exec';
 const form = document.forms['Portfolio-contact-form'];
-                
+
 form.addEventListener('submit', e => { 
     e.preventDefault();
     fetch(scriptURL, {
@@ -18,7 +30,6 @@ function reloadPage() {
 }
 
 /* dark-theme code :
-
 var icon = document.getElementById("moon-icon");
 icon.onclick = function() {
   document.body.classList.toggle("dark-theme");
@@ -29,16 +40,13 @@ icon.onclick = function() {
     icon.src = "Assets/moon.png";
   }
 }
-
 */
 
 /* Toggle-menu code :
-
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
-
 */
